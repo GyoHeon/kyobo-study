6b55cad4c97c2db8dcc76750c2f3be57a1643788
교헌 이 <ghl12078@kyobobook.com>
2024-04-25 15:54:32 +0900

    feat: add initial script

--- one/js/index.js
+++ one/js/index.js
@@ -1,1 +1,23 @@
-console.log('hi man')
+console.log("hi man");
+
+function render() {
+  const root = document.getElementById("app");
+
+  const div = document.createElement("div");
+  div.className = "container";
+
+  div.innerHTML = `
+  <input>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+  </ul>
+  `;
+
+  root.appendChild(div);
+}
+
+render();

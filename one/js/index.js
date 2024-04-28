d750bb5423410754bac0beaad923832acd3974bb
Chohyejin53 <hyejin2338@gmail.com>
2024-04-29 00:51:10 +0900

    add html css

--- one/js/index.js
+++ one/js/index.js
@@ -12,32 +12,98 @@ function render() {
   
   InputArea.innerHTML = `
     <input type="text">
-
+    <button>Add Task</button>
   `;
 
   const TodoListArea = document.createElement("div");
   TodoListArea.className = "todo_area";
   
   TodoListArea.innerHTML = `
-    <ul id="InputList">
-    </ul> 
-  `
-  ;
-
-  const InputList = document.getElementById("InputList");
-
-  const InputListItem = document.createElement("li");
-  InputListItem.className = "InputListItem";
+    <table>
+      <thead>
+        <tr>
+          <th class="number">#</th>
+          <th class="comment">TaskName</th>
+          <th class="state">Status</th>
+          <th class="btn_edit">Edit</th>
+          <th class="btn_remove">Remove</th>
+        </tr>
+      </thead>
+      <tbody Id="InputList">
+        <tr class="InputListItem">
+          <td class="number">24</td> 
+          <td class="comment">Buy Grocery</td> 
+          <td class="state todo">
+            <div>Todo</div>
+          </td> 
+          <td class="btn_edit">
+            <button><img src="./assets/img_edit.png" alt="edit"></button>
+          </td> 
+          <td class="btn_remove">
+            <button><img src="./assets/img_delete.png" alt="delete"></button>
+          </td> 
+        </tr>
+        <tr class="InputListItem">
+          <td class="number">25</td> 
+          <td class="comment">Send Email</td> 
+          <td class="state inProgress">
+            <div>In Progress</div>
+          </td> 
+          <td class="btn_edit">
+            <button><img src="./assets/img_edit.png" alt="edit"></button>
+          </td> 
+          <td class="btn_remove">
+            <button><img src="./assets/img_delete.png" alt="delete"></button>
+          </td> 
+        </tr>
+        <tr class="InputListItem">
+          <td class="number">28</td> 
+          <td class="comment">Finish Assignment</td> 
+          <td class="state complete">
+            <div>Complete</div>
+          </td> 
+          <td class="btn_edit">
+            <button><img src="./assets/img_edit.png" alt="edit"></button>
+          </td> 
+          <td class="btn_remove">
+            <button><img src="./assets/img_delete.png" alt="delete"></button>
+          </td> 
+        </tr>
+        <tr class="InputListItem">
+          <td class="number">30</td> 
+          <td class="comment">Bake Cake</td> 
+          <td class="state todo">
+            <div>Todo</div>
+          </td> 
+          <td class="btn_edit">
+            <button><img src="./assets/img_edit.png" alt="edit"></button>
+          </td> 
+          <td class="btn_remove">
+            <button><img src="./assets/img_delete.png" alt="delete"></button>
+          </td> 
+        </tr>
+        <tr class="InputListItem">
+          <td class="number">31</td> 
+          <td class="comment">Write Blog post</td> 
+          <td class="state inProgressActive">
+            <div>In Progress</div>
+          </td> 
+          <td class="btn_edit">
+            <button><img src="./assets/img_edit.png" alt="edit"></button>
+          </td> 
+          <td class="btn_remove">
+            <button><img src="./assets/img_delete.png" alt="delete"></button>
+          </td> 
+        </tr>
   
-  InputListItem.innerHTML = `
-    <span>가나다라마</span> 
+      </tbody>
+    </table>
   `
   ;
 
   
   root.appendChild(Container);
   Container.append(InputArea, TodoListArea);
-  TodoListArea.append(InputListItem);
   
 }
 

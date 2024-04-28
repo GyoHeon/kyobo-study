5cc2d3e98b9d563a5a3d38a8dca2d7f2becef1f6
Minseok-Choi <cms05041@gmail.com>
2024-04-29 00:08:22 +0900

    feat: index.js 기본 틀 구성

--- one/js/index.js
+++ one/js/index.js
@@ -1,13 +1,45 @@
-console.log("hi man");
 
 function render() {
   const root = document.getElementById("app");
 
-  const div = document.createElement("div");
-  div.className = "container";
+  const header = generateHeader();
+  const footer = generateFooter();
+  const inputForm = generateInputForm();
+  const todoList = generateTodoList();
 
-  div.innerHTML = `
-  <input>
+  root.appendChild(header);
+  root.appendChild(inputForm);
+  root.appendChild(todoList);
+  root.appendChild(footer);
+}
+
+function generateHeader() {
+  const header = document.createElement("header");
+  header.innerHTML = "<h1>Todo List</h1>";
+  return header;
+}
+
+function generateFooter() {
+  const footer = document.createElement("footer");
+  footer.innerHTML = "<p>&copy; minseokchoi js-playgroud</p>";
+  return footer;
+}
+
+function generateInputForm() {
+  const inputForm = document.createElement("form");
+  inputForm.innerHTML = `
+        <input type="text" id="todoInput" placeholder="Add new todo">
+        <button type="submit">Add</button>
+    `;
+
+  return inputForm;
+}
+
+function generateTodoList() {
+  const todoList = document.createElement("div");
+  todoList.className = "todoList";
+
+  todoList.innerHTML = `
   <ul>
     <li>1</li>
     <li>2</li>
@@ -17,7 +49,7 @@ function render() {
   </ul>
   `;
 
-  root.appendChild(div);
+  return todoList;
 }
 
 render();

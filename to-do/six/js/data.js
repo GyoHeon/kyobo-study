31e9016992058c9f1b29bc0fd14a458fc4cb8e86
교헌 이 <ghl12078@gmail.com>
2024-05-20 17:18:44 +0900

    doc: add six directory

---
+++ to-do/six/js/data.js
@@ -0,0 +1,34 @@
+export const todoItems = [
+  {
+    Number: "24",
+    TaskName: "Buy Grocey",
+    status: 0,
+  },
+  {
+    Number: "25",
+    TaskName: "Send Email",
+    status: 1,
+  },
+  {
+    Number: "28",
+    TaskName: "Finish Assignment",
+    status: 2,
+  },
+  {
+    Number: "30",
+    TaskName: "Bake Cake",
+    status: 0,
+  },
+  {
+    Number: "31",
+    TaskName: "Write Blog post",
+    status: 1,
+  },
+];
+
+export const status = ["Todo", "In Progress", "Complete"];
+export const statusClassObj = {
+  Todo: "todo",
+  "In Progress": "inProgress",
+  Complete: "complete",
+};
---
+++ to-do/six/js/render.js
@@ -0,0 +1,47 @@
+import {
+  makeDivWithClass,
+  makeElementWithClass,
+  makeForm,
+  makeHeader,
+  makeListHeader,
+} from "./element.js";
+import { listRender } from "./list.js";
+
+export function render() {
+  const root = document.getElementById("app");
+
+  if (!root) {
+    alert("Root element not found");
+    return;
+  }
+
+  const todo_wrap = makeDivWithClass({
+    className: "todolist_wrap",
+  });
+
+  const headerWrapper = makeHeader();
+  todo_wrap.appendChild(headerWrapper);
+
+  const todoForm = makeForm();
+  todo_wrap.appendChild(todoForm);
+
+  const todo_list_wrap = makeDivWithClass({
+    className: "todolist_list_wrap",
+  });
+  const listHeader = makeListHeader();
+  todo_list_wrap.appendChild(listHeader);
+
+  const todo_list = makeElementWithClass({
+    tag: "ul",
+    className: "todolist_list",
+  });
+
+  todo_list_wrap.appendChild(todo_list);
+  todo_wrap.appendChild(todo_list_wrap);
+  root.appendChild(todo_wrap);
+
+  const listWrapper = document.querySelector("ul.todolist_list");
+  if (listWrapper) {
+    listRender(listWrapper);
+  }
+}
